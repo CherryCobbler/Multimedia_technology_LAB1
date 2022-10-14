359bd2386bdd3c130022c0319315aac4c3cff620
adels <adel.semchenko@gmail.com>
2022-10-14 19:21:45 +0300

    dop (2a)

--- main.py
+++ main.py
@@ -73,6 +73,31 @@ def task3(container1, container2):
         output.mux(packet)
     output.close()
 
+
+def dop(container, x, del_x, y, del_y):
+    count = container.streams.video[0].frames
+    w = container.streams.video[0].codec_context.coded_width
+    h = container.streams.video[0].codec_context.coded_height
+    if x < 0 or del_x <= 0 or y < 0 or del_y <= 0 or x + del_x >= w or y + del_y >= h or x >= w or y >= h:
+        print("Uncorrected data")
+    else:
+        list_frame = []
+        for frame in container.decode(0):
+            rgb = np.asarray(frame.to_rgb().to_image())
+            temp = rgb[x:x + del_x, y:y + del_y]  # i love java but its too cool
+            list_frame.append(av.VideoFrame.from_ndarray(temp, format=str('rgb24')))
+        output_container = av.open('dop.avi', mode='w')
+        output_stream = output_container.add_stream('h264', rate=25)
+        out_packets = []
+        for frame in list_frame:
+            packet = output_stream.encode(frame)
+            out_packets.append(packet)
+        for packet in out_packets:
+            if packet:
+                output_container.mux(packet)
+        output_container.close()
+
+
 container = []
 container.append(av.open('./video/lr1_1.AVI'))
 container.append(av.open('./video/lr1_2.AVI'))
@@ -98,7 +123,7 @@ container.append(av.open('./video/lr1_3.AVI'))
 
 # task3(container[0], container[1])
 
-
+dop(container[0], 50, 150, 60, 100)
 
 container[0].close()
 container[1].close()
